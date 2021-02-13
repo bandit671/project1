e6edaa6624911b3d5473c72c4e03c93090c09299
bandit671 <61143222@g.cmru.ac.th>
2021-02-13 15:22:45 +0700

    สร้าง user ได้แล้ว

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-    }
+        $user = new User();
+        $user->name = "Bandit Bunkamjon";
+        $user->username = "admin";
+        $user->email = "61143222@g.cmru.ac.th";
+        $user->password = Hash::make("1234");
+        $user->save();    }
 }
