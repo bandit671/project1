cb5ada184ef7e55b4858d0d52095a84a65c8a27d
bandit671 <61143222@g.cmru.ac.th>
2021-02-14 11:09:01 +0700

    ทำuser

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,25 +7,51 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-   public function index()
-   {
-       $data = [
-           'categories' => Category::all(),
-       ];
-       return view('category.index', $data);
-   }
-   public function create()
-   {
-       return view('category.create');
-   }
-   public function store(Request $request)
-   {
-       $name = $request->input('name');
+    public function index()
+    {
+        $data = [
+            'categories' => Category::all(),
+        ];
+        return view('category.index', $data);
+    }
+    public function create()
+    {
+        return view('category.create');
+    }
+    public function store(Request $request)
+    {
+        $name = $request->input('name');
 
-       $category = new Category();
-       $category->name = $name;
-       $category->save();
+        $category = new Category();
+        $category->name = $name;
+        $category->save();
 
-       return redirect('/category');
-   }
+        return redirect('/category');
+    }
+
+    public function edit($id)
+    {
+        $category = Category::find($id);
+        $data = [
+            'category' => $category
+        ];
+        return view('category.edit', $data);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $name = $request->input('name');
+
+        $category = Category::find($id);
+        $category->name = $name;
+        $category->save();
+
+        return redirect('/category');
+    }
+    public function delete($id)
+    {
+        $category = Category::find($id);
+        $category->delete();
+        return redirect('/category');
+    }
 }
